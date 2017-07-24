8245782347917e6ce3686f5616c401eb55875948
Jason Wildeman <jw5236@nyu.edu>
2017-07-24 19:16:24 -0400

    update to read/write functionality

--- app/products_app.py
+++ app/products_app.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import getpass
-
+import csv
 user_name = getpass.getuser()
 
 print("----------------------")
@@ -9,12 +8,13 @@ print("----------------------")
 print("Welcome to the products app, " + (user_name) + "!")
 print("")
 
-import csv
-import os
+#
+# Read csv
+#
 
 products = []
 
-csv_file_path = r"C:\Users\Jason\Desktop\python-practice\crud\data\products.csv"
+csv_file_path = r"C:\Users\Jason\Desktop\crud_app_project\data\products.csv"
 
 with open(csv_file_path, "r") as csv_file:
     reader = list(csv.DictReader(csv_file, delimiter=','))
@@ -24,21 +24,18 @@ with open(csv_file_path, "r") as csv_file:
 headers = ["id", "name", "aisle", "department", "price"]
 new_header_info = [header for header in headers if header != "id"]
 
-def get_product_id(product): return int(product["id"])
+def load_id(product):
+    return int(product["id"])
 
 def new_auto_id():
-    product_ids = map(get_product_id, products)
+    product_ids = map(load_id, products)
     return max(product_ids) + 1
 
-# other_path = "C:\Users\Jason\Desktop\python-practice\crud\data\other_products.csv"
-#
-# with open(other_path), "w") as csv_file:
-#     writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
-#     writer.writeheader() # uses fieldnames set above
-#     for product in products:
-#         writer.writerow(product)
-
 print("There are {0} products in the database.".format(len(products))) # .format(len(products)) " + str(len(products)) + " this should return a dynamic count of the products in the database
+
+#
+# menu
+#
 
 menu = """
 
@@ -61,6 +58,10 @@ d["Show"] = 2
 d["Create"] = 3
 d["Update"] = 4
 d["Destroy"] = 5
+
+#
+# Define command operations
+#
 
 def list_products():
     print("")
@@ -109,6 +110,7 @@ def destroy_product():
     print("")
     if product:
         print("Product being destroyed: ", product)
+        del products[products.index(product)]
     else:
         print("Product not found ", product_id)
 
@@ -117,9 +119,11 @@ elif new_operation == "2": show_product()
 elif new_operation == "3": create_product()
 elif new_operation == "4": update_product()
 elif new_operation == "5": destroy_product()
-else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
+else:
+    print("")
+    print("Something's not right. Please choose one of the available commands.")
 
-# if new_operation == "1": # this is where my code originally started
+# if new_operation == "1": # how the code began
 #    print("")
 #    print(" + LISTING PRODUCTS")
 # elif new_operation == "2":
@@ -136,35 +140,16 @@ else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
 #    print(" + DESTROYING A PRODUCT")
 # else:
 #    print("")
-#    print(" + OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED COMMANDS.") # this is where my original code ended
+#    print(" + OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED COMMANDS.")
 
-print("")
+#
+# Write new operations
+#
 
+print(products)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#command = []
-
-#while True:
-#    command = input("Please select a command: ")
-#    if new_operation.title == "Quit":
-#        break
-#    else:
-#        new_operation.append(int(csv_file_path))
-
-#def lookup_product_by_id(product_id):
-#    matching_products = [product for product in products if product["id"] == product_id]
-#    return matching_products[0] # because the line above gives us a list and we want to return a single item.
+with open(csv_file_path, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=headers)
+    writer.writeheader()
+    for product in products:
+        writer.writerow(product)
